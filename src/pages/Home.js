435ef496cdadf8d574cd1556c2063669001ffb63
rodionion <rodionvh@gmail.com>
2025-04-21 12:00:42 -0300

    Add top destinations section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import topRatedDestinationsVictoriaFalls from "../images/top-rated-destinations-victoria-falls.png";
+import topRatedDestinationsSouthLuangwa from "../images/top-rated-destinations-south-luangwa.png";
+import topRatedDestinationsLowZambezi from "../images/top-rated-destinations-low-zambezi.png";
+import topRatedDestinationsKafue from "../images/top-rated-destinations-kafue.png";
 
 function Home() {
   return (
@@ -20,10 +24,47 @@ function Home() {
                         Trusted. Professional. Experienced.
                     </h3>
                 </div>
+            </div>
+
+        </section>
+
+        <section className='top-rated-destinations'>
+        
+            <h2>Top-Rated Destinations</h2>
+
+            <div className="content-container">
+
+                <svg className='dashed-line' width="1512" height="263" viewBox="0 0 1512 263" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 33.3322C77.5385 24.1865 253.362 15.3734 336.349 53.2864C440.083 100.678 559.534 310.197 826.728 250.334C1093.92 190.471 1251.09 -46.4846 1512 10.8837" stroke="white" stroke-width="2.72425" stroke-dasharray="3.63 3.63"/>
+                </svg>
+
+                <div className="item-container victoria-falls">
+                    <p>
+                        Victoria falls - in the local tongue “the smoke that thunders”
+                    </p>
+                    <img className='item-image' src={topRatedDestinationsVictoriaFalls} alt="" />
+                </div>
+
+
+                <div className="item-container south-luangwa">
+                    <p>South Luangwa National Park</p>
+                    <img className="item-image" src={topRatedDestinationsSouthLuangwa} alt="South Luangwa" />
+                </div>
+
+                <div className="item-container low-zambezi">
+                    <p>Low Zambezi National Park</p>
+                    <img className="item-image" src={topRatedDestinationsLowZambezi} alt="Low Zambezi" />
+                </div>
+
+                <div className="item-container kafue">
+                    <p>Kafue National Park</p>
+                    <img className="item-image" src={topRatedDestinationsKafue} alt="Kafue National Park" />
+                </div>
+
+
 
 
             </div>
-
 
         </section>
 
